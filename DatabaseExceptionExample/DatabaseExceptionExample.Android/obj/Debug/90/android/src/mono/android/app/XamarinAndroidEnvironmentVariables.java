b858cb334336f796970f20db35f5e451b695af2a
Nicolai Nebus <nicolainebus@googlemail.com>
2019-06-13 13:24:47 +0200

    Test Deployment to Emulator; Changed MainView-Text

--- DatabaseExceptionExample/DatabaseExceptionExample.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ DatabaseExceptionExample/DatabaseExceptionExample.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "551aabe6-498d-4eeb-af5f-1e097e0a7238",
+		"XAMARIN_BUILD_ID", "7abf1406-3a72-4e4a-ac9c-e6960f211c65",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
